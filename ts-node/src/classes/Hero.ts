fe883d60671df3a6b3a47318e8c8b730b58a2c1c
taylorgr <taylorgr@outlook.com>
2023-06-27 17:18:34 -0600

    seccion 10 ts-node curso typescript

---
+++ ts-node/src/classes/Hero.ts
@@ -0,0 +1,19 @@
+import powers from "../data/powers";
+
+export class Hero {
+  constructor(
+    public name: string,
+    public powerId: number,
+    public age: number
+  ) {}
+
+  get power() {
+    return (
+      powers.find((power) => power.id === this.powerId)?.desc || "not found"
+    );
+  }
+}
+
+export class Hero2 {}
+export class Hero3 {}
+export class Hero4 {}
---
+++ ts-node/src/index.ts
@@ -0,0 +1,7 @@
+import { Hero } from "./classes/Hero";
+// import { Hero as SuperHero } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+
+// const ironman = new HeroClasses.Hero("Ironman", 1, 55);
+const ironman = new Hero("Ironman", 10, 55);
+console.log(ironman.power);
